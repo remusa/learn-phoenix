31e657c76467a31c965ba6fe7032074bf51c3dbf
R Sánchez <rene.as.himself@gmail.com>
2020-05-21 22:55:44 -0500

    Request life-cycle

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -17,6 +17,8 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
