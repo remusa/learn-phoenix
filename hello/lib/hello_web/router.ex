4d7ccd6e604ad2c2d714c1fcd59761a2bb4c26e3
R Sánchez <rene.as.himself@gmail.com>
2020-05-22 13:15:56 -0500

    Routing

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -10,6 +10,11 @@ defmodule HelloWeb.Router do
     plug HelloWeb.Plugs.Locale, "en"
   end
 
+  pipeline :review_checks do
+    plug :ensure_authenticated_user
+    plug :ensure_user_owns_review
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
@@ -18,14 +23,46 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+
+    resources "/users", UserController do
+      resources "/posts", PostController, only: [:index, :show]
+    end
+    resources "/comments", CommentController, except: [:delete]
+
+  end
+
+  scope "/reviews", HelloWeb do
+    pipe_through [:browser, :review_checks]
+
+    resources "/", ReviewController
+  end
+
+  scope "/" do
+    pipe_through [:authenticate_user, :ensure_admin]
+    forward "/jobs", BackgroundJob.Plug, name: "Hello Phoenix"
+  end
+
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/images", ImageController
+    resources "/reviews", ReviewController
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HelloWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HelloWeb.Api, as: :api do
+    pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+      resources "/images", ImageController
+      resources "/reviews", ReviewController
+      resources "/users", UserController
+    end
+  end
 
   # Enables LiveDashboard only for development
   #
