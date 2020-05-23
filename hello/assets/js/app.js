692dcc8b90b8a2d45813bdf82beb2e6c2aefba8c
R Sánchez <rene.as.himself@gmail.com>
2020-05-23 16:16:20 -0500

    Deployment

--- hello/assets/js/app.js
+++ hello/assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import '../css/app.scss'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,10 +12,10 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import 'phoenix_html'
 
 import { Socket, Presence } from 'phoenix'
-import socket from './socket'
+// import socket from './socket'
 
 let socket = new Socket('/socket', {
   params: { user_id: window.location.search.split('=')[1] },
