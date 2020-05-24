3306615c30789fc1d5042d1987fb66fec4916573
R Sánchez <rene.as.himself@gmail.com>
2020-05-24 13:34:37 -0500

    Step 13: Share State Between Clients!

--- live_view_counter/lib/live_view_counter_web/live/counter.ex
+++ live_view_counter/lib/live_view_counter_web/live/counter.ex
@@ -1,16 +1,28 @@
 defmodule LiveViewCounterWeb.Counter do
   use Phoenix.LiveView
 
-  def mount(_params, _session, socket) do
+  @topic "live"
+
+  def mount(_session, _params, socket) do
+    # subscribe to the channel
+    LiveViewCounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event("inc", _value, socket) do
+    new_state = update(socket, :val, &(&1 + 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+    new_state = update(socket, :val, &(&1 - 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
