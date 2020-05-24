e51ad9377f074a30aae855db6af9212dafa0f42c
R Sánchez <rene.as.himself@gmail.com>
2020-05-24 13:00:19 -0500

    Step 2: Add LiveView to deps in mix.exs File

--- live_view_counter/mix.exs
+++ live_view_counter/mix.exs
@@ -41,7 +41,8 @@ defmodule LiveViewCounter.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_live_view, "~> 0.10.0"},
     ]
   end
 
